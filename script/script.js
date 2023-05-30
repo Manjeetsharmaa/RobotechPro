4f147353f0b6f48f80a3b18c378b21d24160fade
Manjeet <m4sharma8860@gmail.com>
2023-05-30 13:11:01 +0530

    ADDED SOME COOL CONTENT

--- script/script.js
+++ script/script.js
@@ -1,24 +1,53 @@
+
+
+document.getElementById("iso-btn").onclick = function () {
+  let showImage = document.querySelector(".show-hide");
+  if (showImage.classList.toggle('active')) {
+    console.log(`it's working`)
+  } else {
+    showImage.classList.remove('remove');
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const swiper = new Swiper('.swiper', {
 
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: 'false'
-    },
-    // Optional parameters
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: 'true'
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-   
-  });
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: 'false'
+  },
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: 'true'
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+
+});
